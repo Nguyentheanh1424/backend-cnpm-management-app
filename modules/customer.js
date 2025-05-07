b61985e2018462f8ff328e4445592f1133fcea49
CX330 <quanghuy11102k4@gmail.com>
2025-05-08 03:50:05 +0700

    productSchema

---
+++ modules/customer.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const customerSchema = new mongoose.Schema({
+    name: { type: String},
+    phone: { type: String, required: true },
+    email: { type: String},
+    rate: { type: Number,default:0},
+    creater: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Users',
+        required: true
+    },
+    money:{
+        type:String,
+        default:"0.000"
+    },
+    lastPurchaseDate: { type: Date ,default:null},
+    firstPurchaseDate: { type: Date ,default:null},
+}, { timestamps: true });
+
+const Customer = mongoose.model('Customers', customerSchema,'Customers');
+module.exports = Customer;
