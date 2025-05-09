92efbd224bebac060f84eb5a586727cbbfdfa844
Nguyen The Anh <ntheanh0104@gmail.com>
2025-05-09 23:57:21 +0700

    Logger, config database, swagger.

---
+++ config/dbconfig.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const logger = require('../config/logger');
+const mongoose = require("mongoose");
+
+class Database {
+    constructor() {
+        this.uri = process.env.MONGO_URI;
+    }
+
+    async connect() {
+        try {
+            logger.info(`Attempting to connect to MongoDB Atlas at ${this.uri.replace(/:.*@/, ':****@')}`);
+            await mongoose.connect(this.uri)
+            logger.info(`Successfully connected to MongoDB Atlas`);
+        }
+        catch (err) {
+            logger.error(`MongoDB Atlas connection error: ${err.message}`);
+            throw new Error(`Failed to connect to MongoDB Atlas`);
+        }
+    }
+}
+
+module.exports = new Database();
