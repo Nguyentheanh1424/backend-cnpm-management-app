7eaddc3935b1c27393b6ec8114c25dd0f76658c2
CX330 <quanghuy11102k4@gmail.com>
2025-05-09 22:11:26 +0700

    productSchema

--- modules/products.js
+++ modules/products.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const productSchema = new mongoose.Schema({
     owner:{
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
+        ref: 'Users',
         required: true
     },
     name: { type: String, required: true},
